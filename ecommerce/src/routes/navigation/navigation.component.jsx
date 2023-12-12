8b06a2339b31d280f6cf20d30e1e8c67366cae2c
officialgio121 <officialgio121@gmail.com>
2023-12-12 16:13:11 -0600

    Added Routing with React-Router

---
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+import './navigation.styles.scss';
+
+// This is our top-level component
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo"/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      {/* Outlet will renders those components depending on the route */}
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
