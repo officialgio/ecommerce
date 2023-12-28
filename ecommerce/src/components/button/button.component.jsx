e7fe080c52b75218786d29cac445f7ee5e485298
officialgio121 <officialgio121@gmail.com>
2023-12-28 15:33:25 -0600

    Added Styled-Components - Button

--- ecommerce/src/components/button/button.component.jsx
+++ ecommerce/src/components/button/button.component.jsx
@@ -1,19 +1,28 @@
-import './button.styles.scss';
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
-const BUTTON_TYPE_CLASSES = {
+// Object to leverage to use different types of button styles
+// Which will be used in getButton()
+export const BUTTON_TYPE_CLASSES = {
+  base: "based",
   google: "google-sign-in",
   inverted: "inverted",
 };
 
+// Based on the type that is passed render the corresponding styled component
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
+
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button
-      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+  const CustomButton = getButton(buttonType);
+  return <CustomButton {...otherProps}>{children}</CustomButton>;
 };
 
 export default Button;
--- ecommerce/src/routes/navigation/navigation.styles.jsx
+++ ecommerce/src/routes/navigation/navigation.styles.jsx
@@ -27,18 +27,3 @@ export const NavLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
 `;
-
-// .navigation {
-
-//     .nav-links-container {
-//         width: 50%;
-//         height: 100%;
-//         display: flex;
-//         align-items: center;
-//         justify-content: flex-end;
-
-//         .nav-link {
-//             padding: 10px 15px;
-//             cursor: pointer;
-//         }
-//     }
