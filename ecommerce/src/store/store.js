8ac45199e26c9a2ccc2c21ebfadcecd445fedc4d
officialgio121 <officialgio121@gmail.com>
2024-01-02 17:09:01 -0600

    Added Setting up our Store

--- ecommerce/src/store/store.js
+++ ecommerce/src/store/store.js
@@ -1,4 +1,11 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+// runs before an action hits a reducer
+// This can take many types of middleware. Therefore, ensure to compose.
+const middleware = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleware));
 
 // root reducer
+export const store = createStore(rootReducer, undefined, composedEnhancers);
