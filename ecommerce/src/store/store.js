91035262c10b5abccff7a3563704aefe84ec7bee
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2024-09-10 13:07:51 -0500

    Added Redux Toolkit Intro

--- ecommerce/src/store/store.js
+++ ecommerce/src/store/store.js
@@ -5,37 +5,38 @@ import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["user"], // blaclist these reducers
-};
+import configureStore from "@reduxjs/toolkit";
 
-// Setup persisted reducer for the root reducer
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["user"], // blaclist these reducers
+// };
+
+// // Setup persisted reducer for the root reducer
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // apply any middleware in the right env
 const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
   Boolean
 );
 
-// apply redux devtools in the right env
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// // apply redux devtools in the right env
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-// This will take either compose or some composeEnhancer
-// If you're adding additional middleware, set up the enhancers accordingly.
-const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+// // This will take either compose or some composeEnhancer
+// // If you're adding additional middleware, set up the enhancers accordingly.
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 // root reducer with redux add-ons
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  // middleware: middlewares,
+});
 
-// The main store that will be available in index react app.
-export const persistor = persistStore(store);
+// // The main store that will be available in index react app.
+// export const persistor = persistStore(store);
