9d466181b2884ba86f1d93db941bba35ea0c222d
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2024-09-17 13:16:27 -0500

    Removed unecessary code

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -14,7 +14,6 @@ import { stripePromise } from "./utils/stripe/stripe.util";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
   <Provider store={store}>
     {/* <PersistGate persistor={persistor}> */}
     <BrowserRouter>
@@ -24,7 +23,6 @@ root.render(
     </BrowserRouter>
     {/* </PersistGate> */}
   </Provider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
