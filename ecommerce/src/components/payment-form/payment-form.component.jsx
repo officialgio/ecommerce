fcb008adb06dbdac44b6d0a2150a5e1dfd73fa7e
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2024-09-17 11:57:28 -0500

    Added Writing Serverless Function

--- ecommerce/src/components/payment-form/payment-form.component.jsx
+++ ecommerce/src/components/payment-form/payment-form.component.jsx
@@ -10,14 +10,43 @@ const PaymentForm = () => {
     e.preventDefault();
 
     if (!stripe || !elements) {
-        return;
+      return;
     }
 
-    
+    // One a request is sent, we get many
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Giovanny Hernandez",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("done payment!");
+      }
+    }
   };
 
   return (
-    <PaymentFormContainer>
+    <PaymentFormContainer onSubmit={paymentHandler}>
       <FormContainer>
         <h2>Credit Card Payment: </h2>
         <CardElement />
